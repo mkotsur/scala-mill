c4cb2bc95aa5a376fa73f427c9574820d97b337e
Mike Kotsur <miccots@gmail.com>
2020-08-17 21:15:27 +0200

    Push into Docker registry

--- publish.sh
+++ publish.sh
@@ -2,10 +2,14 @@
 
 SCALA_VERSION=${1:-"2.13.3"} 
 MILL_VERSION=${1:-"0.8.0"} 
+TAG="$SCALA_VERSION"_"$MILL_VERSION"
 
 echo "Building image with Scala $SCALA_VERSION and Mille $MILL_VERSION"
 
 docker build . \
 --build-arg SCALA_VERSION=$SCALA_VERSION  \
 --build-arg MILL_VERSION=$MILL_VERSION  \
--t mkotsur/scala-mill:"$SCALA_VERSION"_"$MILL_VERSION"
+-t mkotsur/scala-mill:$TAG
+
+docker push mkotsur/scala-mill:latest
+docker push mkotsur/scala-mill:$TAG
