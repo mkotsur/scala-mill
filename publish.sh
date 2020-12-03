82b84e3d8cf612dbaa27e8b1d208934f42ba0978
Mike Kotsur <miccots@gmail.com>
2020-12-03 11:42:47 +0100

    Adding latest tag

--- publish.sh
+++ publish.sh
@@ -4,12 +4,13 @@ SCALA_VERSION=${1:-"2.13.3"}
 MILL_VERSION=${1:-"0.9.3"} 
 TAG="$SCALA_VERSION"_"$MILL_VERSION"
 
-echo "Building image with Scala $SCALA_VERSION and Mille $MILL_VERSION"
+echo "Building image with Scala $SCALA_VERSION and Mill $MILL_VERSION"
 
 docker build . \
 --build-arg SCALA_VERSION=$SCALA_VERSION  \
 --build-arg MILL_VERSION=$MILL_VERSION  \
--t mkotsur/scala-mill:$TAG
+-t mkotsur/scala-mill:$TAG \
+-t mkotsur/scala-mill:latest
 
 docker push mkotsur/scala-mill:latest
 docker push mkotsur/scala-mill:$TAG
